3087ba7be955d3aa1e9206e9fa8371dd4b0859b0
GeedipalliVasu <vasu,geedipalli2005@gmail.com>
2026-05-25 23:18:40 +0530

    Implemented embedding

--- rag-backend/app/routes/upload.py
+++ rag-backend/app/routes/upload.py
@@ -3,6 +3,8 @@ import os
 
 from app.ingestion.pdf_parser import extract_text_from_pdf
 from app.ingestion.chunker import chunk_text
+from app.retrieval.embedder import generate_embeddings
+from app.retrieval.vector_store import create_faiss_index
 
 router = APIRouter()
 
@@ -25,8 +27,14 @@ async def upload_pdf(file: UploadFile = File(...)):
 
     chunks=chunk_text(extracted_text)
 
+    embeddings = generate_embeddings(chunks)
+
+    index = create_faiss_index(embeddings)
+
     return {
         "filename": file.filename,
         "text_preview": extracted_text[:1000],
-        "sample_chunk": chunks[0]
+        "sample_chunk": chunks[0],
+        "embedding_dimension": embeddings.shape[1],
+        "faiss_vectors":index.ntotal
     }
